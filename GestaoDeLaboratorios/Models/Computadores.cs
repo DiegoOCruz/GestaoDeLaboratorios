210e1cfa907b5baad143372c0fad94425bc79e4d
diego <diego@DIEGO_PC>
2024-03-06 15:40:05 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set;}
+        [Required]
+        public string Processador { get; set;}
+        [Required]
+        public string PlacaMae { get; set;}
+        public int Memoria { get; set;}
+        public int HD { get; set;}
+        [Required]
+        public int NumeroPratrimonio { get; set; }
+        public string DataDeCompra { get; set; }
+
+    }
+}
